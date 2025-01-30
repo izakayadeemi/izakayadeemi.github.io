a7071ab2907717ee4a25b5871dd6a18025330ed1
zerodea <zerodea@posteo.com>
2025-01-30 12:11:36 +0100

    moving stuff around and shit

--- js/languageSelector.js
+++ js/languageSelector.js
@@ -1,29 +1,25 @@
-function changeLanguage() {
-    document.getElementById("languageSelector").onchange = changeListener;
-    document.getElementById("languageSelector").addEventListener('change', changeListener)
+console.log("logging")
+document.getElementById("languageSelector").onchange = function () {
+    console.log("changeListener")
+    var value = this.value
+    console.log(value);
 
-    function changeListener() {
-        console.log("changeListener")
-        var value = this.value
-        console.log(value);
+    var curLocation = window.location.href
+    console.log(curLocation);
 
-        var curLocation = window.location.href
-        console.log(curLocation);
-
-        var newLocation = curLocation.split("/").map((s, i) => {
-            let retV;
-            if (i == 4) {
-                retV = value
-            } else {
-                retV = s
-            }
-            return retV
-        }).join("/")
-        console.log(newLocation);
-
-        if (newLocation != curLocation) {
-            console.log("opening newLocation");
-            window.open(document.URL)
+    var newLocation = curLocation.split("/").map((s, i) => {
+        let retV;
+        if (i == 4) {
+            retV = value
+        } else {
+            retV = s
         }
+        return retV
+    }).join("/")
+    console.log(newLocation);
+
+    if (newLocation != curLocation) {
+        console.log("opening newLocation");
+        window.open(document.URL)
     }
 }
