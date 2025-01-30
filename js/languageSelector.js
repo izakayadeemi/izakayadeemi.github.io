5c9180ef987635a4a6f69d6c3e9aa3800107717d
zerodea <zerodea@posteo.com>
2025-01-30 16:37:23 +0100

    try to translate  the text and fix a bug with featured recipe titles

--- js/languageSelector.js
+++ js/languageSelector.js
@@ -1,18 +1,37 @@
 console.log("logging")
+
+function translate(text, curLang, selectedLang){
+    const translatePromise = fetch(`strings_${curLang}.txt`)
+        .then((res) => res.text())
+        .then((text) => text.split("\n").indexOf(text))
+        // .then((index) => fetch(`strings_${selectedLang}.txt`)
+        //         .then((res) => res.text())
+        //         .then((text) => text.split("\n").at(index))
+        //         .catch((e) => console.error(e))
+        // )
+        // .catch((e) => console.error(e));
+    return Promise.resolve(translatePromise)
+}
+
 document.getElementById("languageSelector").onchange = function () {
     console.log("changeListener")
-    var value = this.value
-    console.log(value);
+    const selectedLang = this.value
+    console.log(selectedLang);
 
-    var curLocation = window.location.href
+    const curLocation = window.location.href;
     console.log(curLocation);
 
-    var newLocation = curLocation.split("/").map((s, i) => {
+    let curLang;
+
+    let newLocation = curLocation.split("/").map((s, i) => {
         let retV;
-        if (i == 3) {
-            retV = value
+        if (i < 3) {
+           retV = s
+        } else if (i == 3) {
+            curLang = s
+            retV = selectedLang
         } else {
-            retV = s
+            retV = translate(s, curLang, selectedLang)
         }
         return retV
     }).join("/")
